a5eeab277aeda6f49107501333d67bc8b5fe0348
Sam Sweigart <samuel.sweigart@gmail.com>
2021-08-03 17:56:18 -0400

    landing page / register page / and react router functioning

---
+++ client/src/components/Home/index.js
@@ -0,0 +1,38 @@
+import './index.css';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <div className='main-container'>
+            <div className="welcome">
+                <h2>Welcome</h2>
+            </div>
+            <main className="card">
+
+                <form>
+                    <div className="email">
+                        <input placeholder='E-mail'></input>
+                        <i className='fas fa-user-alt email-icon' aria-hidden='true'></i>
+                    </div>
+
+                    <div className="password">
+                        <input placeholder='Password'></input>
+                        <i class="fas fa-lock password-icon" aria-hidden='true'></i>
+                    </div>
+                    
+                    <button type='submit' className='submit-btn'>
+                        Sign in
+                    </button>
+                </form>
+                <div className="register">
+                    <p>
+                        New around here? 
+                        <Link to='/register'>Sign Up!</Link>
+                    </p>
+                </div>
+            </main>
+        </div>
+    );
+};
+ 
+export default Home;
