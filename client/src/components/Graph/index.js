b906240313eb4cbb95b6e39757b9c301d7fadee7
Michael DiSanto <mdisanto928@gmail.com>
2021-08-05 22:47:24 -0400

    default graph now rendering

--- client/src/components/Graph/index.js
+++ client/src/components/Graph/index.js
@@ -1,13 +1,71 @@
 import './index.css';
 import navBar from '../NavBar';
+import * as React from "react";
+import { Chart } from "react-google-charts";
+
+// const data = [
+//     ["Year", "Sales", "Expenses"],
+//     ["2004", 1000, 400],
+//     ["2005", 1170, 460],
+//     ["2006", 660, 1120],
+//     ["2008", 1030, 540],
+//     ["2009", 1000, 400],
+//     ["2010", 1170, 460],
+//     ["2011", 660, 1120],
+//     ["2012", 1030, 540]
+//   ];
+
+//   const options = {
+//     title: "My Tips",
+//     curveType: "function",
+//     legend: { position: "bottom" }
+//   };
+
 
 const Graph = () => {
     return (
         <div className='graph'>
             <h2>Graph</h2>
             { navBar }
+        {/* <div className="App">
+        <Chart
+          chartType="ScatterChart"
+          width="80%"
+          height="400px"
+          data={data}
+          options={options}
+          legendToggle
+        />
+      </div> */}
+      <div>
+      <Chart
+  width={'600px'}
+  height={'400px'}
+  chartType="ScatterChart"
+  loader={<div>Loading Chart</div>}
+  data={[
+    ['Age', 'Tips'],
+    [8, 12],
+    [4, 5.5],
+    [11, 14],
+    [4, 5],
+    [3, 3.5],
+    [6.5, 7],
+  ]}
+  options={{
+    title: 'Age vs. Weight comparison',
+    hAxis: { title: 'Age', minValue: 0, maxValue: 15 },
+    vAxis: { title: 'Tips', minValue: 0, maxValue: 15 },
+    legend: 'none',
+  }}
+  rootProps={{ 'data-testid': '1' }}
+/>
+      </div>
         </div>
+        
     )
 }
+
+
 
 export default Graph
