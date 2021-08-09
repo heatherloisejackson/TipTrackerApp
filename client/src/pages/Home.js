4b9754952336cf901248812aa3f3f53978b46f20
Alex Bradshaw <alexanderbradshaw5@gmail.com>
2021-08-09 17:53:02 -0400

    changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,14 +2,14 @@ import React from 'react';
 // Import the `useQuery()` hook from Apollo Client
 import { useQuery } from '@apollo/client';
 
-import ThoughtList from '../components/ThoughtList';
+import Home from '../components/Home/index';
 
 // Import the query we are going to execute from its file
 import { QUERY_THOUGHTS } from '../utils/queries';
 
 const Home = () => {
   // Execute the query on component load
-  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  const { loading, data } = useQuery(QUERY_TRANSACTIONS);
 
   // Use optional chaining to check if data exists and if it has a thoughts property. If not, return an empty array to use.
   const thoughts = data?.thoughts || [];
@@ -22,10 +22,7 @@ const Home = () => {
           {loading ? (
             <div>Loading...</div>
           ) : (
-            <ThoughtList
-              thoughts={thoughts}
-              title="Some Feed for Thought(s)..."
-            />
+            <Home/>
           )}
         </div>
       </div>
