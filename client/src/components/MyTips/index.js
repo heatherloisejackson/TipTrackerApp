c4e7d846ef6307742fe79d02b04e111fcee63393
Heather Jackson <hmjackson917@gmail.com>
2021-08-11 22:04:53 -0400

    nav bar not showing up on login page anymore

--- client/src/components/MyTips/index.js
+++ client/src/components/MyTips/index.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import ReactCalendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './index.css';
-import navBar from '../NavBar';
+import NavBar from '../NavBar';
 import TipEntry from '../TipModal';
 import moment from 'moment';
+
 const Calendar = (props) => {
     const [show, setShow] = useState(false);
     const [date, onChange] = useState(new Date()/* moment().format("MMM Do YY") */);
@@ -25,7 +26,7 @@ const Calendar = (props) => {
                 <ReactCalendar onChange={onChange} date={date} onClickDay={toggleShowModal} tileContent={``}/>
             </div>
             {showModal ? <TipEntry date={date} onChange={onChange} toggleShowModal={toggleShowModal} date={date}/* value={value} *//> : <></> }
-            { navBar }
+            <NavBar/>
         </div>
     )
 };
