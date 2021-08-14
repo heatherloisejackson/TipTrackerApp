e2a1eac8ac302a63b9c82392aa04b36a26787ea9
Sam Sweigart <samuel.sweigart@gmail.com>
2021-08-14 14:53:18 -0400

    login / register page styling

--- client/src/components/MyTips/index.js
+++ client/src/components/MyTips/index.js
@@ -7,11 +7,14 @@ import TipEntry from '../TipModal';
 import moment from 'moment';
 
 const Calendar = (props) => {
+    const [show, setShow] = useState(false);
     const [date, onChange] = useState(new Date()/* moment().format("MMM Do YY") */);
     const [showModal, setShowModal] = useState(false);
 
 
     const toggleShowModal = (e, value) => {
+        //const newShowModal = !showModal;
+        //console.log('Date:', date)
         setShowModal(!showModal)
     }
 
@@ -20,7 +23,7 @@ const Calendar = (props) => {
         <div className='calendar-container'>
             <h2>My Tips</h2>
             <div className='calendar'>
-                <ReactCalendar onChange={onChange} date={date} onClickDay={toggleShowModal} tileContent={``}/>
+                <ReactCalendar onChange={onChange} tileContent={"pog"} date={date} onClickDay={toggleShowModal} tileContent={``}/>
             </div>
 
             {showModal ? <TipEntry date={date} onChange={onChange} toggleShowModal={toggleShowModal} date={date}/* value={value} *//> : <></> }
